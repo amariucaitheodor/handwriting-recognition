7ca4885579cd312e31421a78fa7e792f812a60e9
Theodor <32778667+theodor1289@users.noreply.github.com>
2019-04-04 16:34:18 +0100

    Urgent fix

--- Tasks/task1_3.m
+++ Tasks/task1_3.m
@@ -21,19 +21,19 @@ CumVarPercent = CumVar/sum(EVals);
 MinDims = zeros(4,1);
 for i=1:size(CumVarPercent,1)
     if CumVarPercent(i)>=0.7*CumVarPercent(size(CumVarPercent,1)) && MinDims(1)==0
-        MinDims(1) = CumVarPercent(i);
+        MinDims(1) = i;
     end
     
     if CumVarPercent(i)>=0.8*CumVarPercent(size(CumVarPercent,1)) && MinDims(2)==0
-        MinDims(2) = CumVarPercent(i);
+        MinDims(2) = i;
     end
     
     if CumVarPercent(i)>=0.9*CumVarPercent(size(CumVarPercent,1)) && MinDims(3)==0
-        MinDims(3) = CumVarPercent(i);
+        MinDims(3) = i;
     end
     
     if CumVarPercent(i)>=0.95*CumVarPercent(size(CumVarPercent,1)) && MinDims(4)==0
-        MinDims(4) = CumVarPercent(i);
+        MinDims(4) = i;
     end 
 end
 
